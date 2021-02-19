29df9767a1d6a3205c8567b33988c59eccf772b2
Ztarby Xtar <ztarbyxtar@gmail.com>
2021-02-18 21:48:56 -0600

    add a little thing

--- Petal/Petal.cpp
+++ Petal/Petal.cpp
@@ -7,10 +7,11 @@ using namespace std;
 
 int main()
 {
-	tstt<int> tryit;
-	//cout << "Main console, example app" << endl;
-	auto a = tryit.test2();
-	cout << a;
+	
+	cout << "Main console, example app" << endl;
+	
+
+	cout << "(o゜▽゜)o☆ end /n";
 /*
 	int x = 100;
 	int* a;
